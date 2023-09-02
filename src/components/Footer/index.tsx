5c688a0989abf56059e2b10ced3366ae7763cc19
hjy0951 <hjy0951@gmail.com>
2023-09-02 23:57:34 +0900

    feat: footer 텍스트 위치 가운데로 조정

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,17 @@
+import styled from "@emotion/styled";
+
+const StyledFooter = styled.footer`
+  width: 100%;
+  height: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 export const Footer = () => {
   return (
-    <footer>
+    <StyledFooter>
       © {new Date().getFullYear()}. Heojoooon all rights reserved.
-    </footer>
+    </StyledFooter>
   );
 };
