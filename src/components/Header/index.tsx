9652905b86b602c576912b72d32c338f701cae90
hjy0951 <hjy0951@gmail.com>
2023-09-04 20:34:50 +0900

    feat: 내 SNS 이동 링크 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,13 +11,30 @@ const HeaderLayout = styled.header`
   align-items: center;
 `;
 
+const Buttons = styled.div`
+  width: 30%;
+  display: flex;
+  justify-content: space-evenly;
+`;
+
 export const Header = () => {
   return (
     <HeaderLayout>
       <a href="/">
         <p>Heojoooon</p>
       </a>
-      <button>Mode</button>
+      <Buttons>
+        <a href="https://github.com/hjy0951">
+          <p>GitHub</p>
+        </a>
+        <a href="https://www.linkedin.com/in/%EC%A4%80%EC%98%81-%ED%97%88-159288250/">
+          <p>LinkedIn</p>
+        </a>
+        <a href="https://www.instagram.com/heojoooon/">
+          <p>instagram</p>
+        </a>
+        <button>Mode</button>
+      </Buttons>
     </HeaderLayout>
   );
 };
