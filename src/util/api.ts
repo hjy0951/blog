ab38c2a3d9be7ab922977046cab177c0950b8ca7
hjy0951 <hjy0951@gmail.com>
2023-09-05 15:35:12 +0900

    feat: 정적 파일을 불러오기 위한 로직 추가 (next 공식 문서 예제 참고)

---
+++ src/util/api.ts
@@ -0,0 +1,64 @@
+// 공식문서 예제의 "blog-start" code 참고
+import fs from "fs";
+import { join } from "path";
+import matter from "gray-matter";
+
+const postsDirectory = join(process.cwd(), "/contents");
+
+export function getPostTags() {
+  const ret = fs.readdirSync(postsDirectory);
+  console.log(ret);
+  return ret;
+}
+
+export function getPostSlugs(tags: string[]) {
+  let totalSlugs: string[] = [];
+  tags.forEach((tag) => {
+    const dirPath = join(postsDirectory, tag);
+    const slugs = fs.readdirSync(dirPath).map((el) => `${tag}/${el}`);
+    totalSlugs = [...slugs];
+  });
+  // return fs.readdirSync(postsDirectory);
+  return totalSlugs;
+}
+
+export function getPostBySlug(slug: string, fields: string[] = []) {
+  const realSlug = slug.replace(/\.mdx$/, "");
+  const fullPath = join(postsDirectory, `${realSlug}/content.mdx`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+  const { data, content } = matter(fileContents);
+
+  type Items = {
+    [key: string]: string;
+  };
+
+  const items: Items = {};
+
+  // Ensure only the minimal needed data is exposed
+  fields.forEach((field) => {
+    if (field === "slug") {
+      items[field] = realSlug;
+    }
+    if (field === "content") {
+      items[field] = content;
+    }
+
+    if (typeof data[field] !== "undefined") {
+      items[field] = data[field];
+    }
+  });
+
+  return items;
+}
+
+export function getAllPosts(fields: string[] = []) {
+  console.log("asdasdad");
+  const tags = getPostTags();
+  const slugs = getPostSlugs(tags);
+  console.log(slugs);
+  const posts = slugs
+    .map((slug) => getPostBySlug(slug, fields))
+    // sort posts by date in descending order
+    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
+  return posts;
+}
