b5d40b3876abd19edbe22af2051cba6ad350b111
hjy0951 <hjy0951@gmail.com>
2023-09-06 16:00:49 +0900

    feat: tag 목록을 보여주기 위한 임시 컴포넌트 추가

--- src/components/Body/TagList.tsx
+++ src/components/Body/TagList.tsx
@@ -3,13 +3,24 @@ interface Props {
 }
 
 export const Tags = ({ tagData }: Props) => {
+  const totalPostCount = tagData.reduce((acc, el) => {
+    return acc + el[1];
+  }, 0);
+  const tagList = [["All", totalPostCount]].concat(tagData);
+  const createTag = (data: (string | number)[], idx: number) => {
+    return (
+      <div key={idx} className="flex hover:font-bold">
+        <h3>{data[0]}</h3>
+        <p>{"(" + data[1] + ")"}</p>
+      </div>
+    );
+  };
+
   return (
     <div className="flex flex-col items-center">
-      <div className="flex gap-2 justify-center">
-        <h2>currentTags</h2>
-        <p>{"(" + ")"}</p>
+      <div className="flex gap-2">
+        {tagList.map((tag, idx) => createTag(tag, idx))}
       </div>
-      {/* <div>{tags}</div> */}
     </div>
   );
 };
