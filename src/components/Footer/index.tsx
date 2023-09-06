20668d7bf1dba717aaf6736a20250a7dca5ead4b
hjy0951 <hjy0951@gmail.com>
2023-09-07 00:55:38 +0900

    design: footer 크기 조정

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="w-full h-12 flex items-center justify-center">
+    <footer className="w-full h-24 flex items-center justify-center">
       © {new Date().getFullYear()}. Heojoooon all rights reserved.
     </footer>
   );
