379cbb9aadd31fbb9bfc284f9c5a314ea7097ae9
hjy0951 <hjy0951@gmail.com>
2023-09-08 17:08:18 +0900

    feat: Post Card의 이미지만이 아닌 card 어느 부분이 hover 되더라도 description이 나오도록 설정

--- src/components/Body/PostCard.tsx
+++ src/components/Body/PostCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import React, { useState } from "react";
 
 interface Props {
   slug: string;
@@ -15,25 +16,39 @@ export const PostCard = ({
   tags,
   createdAt,
 }: Props) => {
+  const [isHover, setIsHover] = useState<boolean>(false);
   return (
-    <article className="pt-4 pb-4 rounded-md border border-solid shadow-lg">
-      <div className="flex flex-col gap-6 m-6">
-        <div className="absolute w-108 h-72 overflow-hidden bg-[hsl(0,0%,10%,0.4)] bg-fixed opacity-0 transition hover:opacity-100"></div>
-        <Image
-          className="rounded-md"
-          src={`/contents/${tags[0]}/${slug}/cover.png`}
-          width={432}
-          height={288}
-          alt={slug + " thumbnail"}
-        />
-        <div className="flex flex-col items-end">
-          <p>{title}</p>
-          <p>{createdAt}</p>
-        </div>
+    <a
+      href="/"
+      onMouseOver={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
+      <article className="pt-4 pb-4 rounded-md border border-solid shadow-lg">
+        <div className="flex flex-col gap-6 m-6">
+          <div
+            className="absolute w-108 h-72 rounded-md shadow overflow-hidden bg-[hsl(0,0%,10%,0.4)] bg-fixed transition
+            opacity-0 hover:opacity-100
+            "
+            style={{ opacity: isHover ? 1 : 0 }}
+          >
+            <p className="text-white">{description}</p>
+          </div>
+          <Image
+            className="rounded-md"
+            src={`/contents/${tags[0]}/${slug}/cover.png`}
+            width={432}
+            height={288}
+            alt={slug + " thumbnail"}
+          />
+          <div className="flex flex-col items-end">
+            <p>{title}</p>
+            <p>{createdAt}</p>
+          </div>
 
-        {/* <p>{description}</p> */}
-      </div>
-    </article>
+          {/* <p>{description}</p> */}
+        </div>
+      </article>
+    </a>
   );
 };
 /*
