0a012eafcd99f154ee87175bf1591e5930f218a9
hjy0951 <hjy0951@gmail.com>
2023-09-20 16:18:29 +0900

    feat: 404 Error Page custom

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,27 @@
+import Image from "next/image";
 import Link from "next/link";
+import NotFoundSvg from "@/assets/404.svg";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="pt-12 flex flex-col items-center">
+      <section className="flex flex-col gap-10 items-center">
+        <Image
+          src={NotFoundSvg}
+          width={432}
+          height={288}
+          alt="Page is not found!"
+          priority
+        />
+        <div className="flex flex-col items-end">
+          <p>페이지를 찾을 수 없습니다.</p>
+          <p className="text-xs">{'"Page is not found."'}</p>
+        </div>
+        <Link className="flex flex-col items-end hover:animate-bounce" href="/">
+          <p>홈으로 이동</p>
+          <p className="text-xs">{'"Go home"'}</p>
+        </Link>
+      </section>
     </div>
   );
 }
