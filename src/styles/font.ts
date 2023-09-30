f6bac2ad9f4627f7f79c7cb0ba8ebe6bfa3179a4
hjy0951 <hjy0951@gmail.com>
2023-09-30 19:40:40 +0900

    refactor: fonts 폴더 위치 변경

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -3,42 +3,42 @@ import localFont from "next/font/local";
 export const suite = localFont({
   src: [
     {
-      path: "./fonts/Suite/SUITE-Light.woff2",
+      path: "../assets/fonts/Suite/SUITE-Light.woff2",
       weight: "300",
       style: "normal",
     },
     {
-      path: "./fonts/Suite/SUITE-Regular.woff2",
+      path: "../assets/fonts/Suite/SUITE-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./fonts/Suite/SUITE-Medium.woff2",
+      path: "../assets/fonts/Suite/SUITE-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "./fonts/Suite/SUITE-SemiBold.woff2",
+      path: "../assets/fonts/Suite/SUITE-SemiBold.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "./fonts/Suite/SUITE-Bold.woff2",
+      path: "../assets/fonts/Suite/SUITE-Bold.woff2",
       weight: "700",
       style: "normal",
     },
     {
-      path: "./fonts/Suite/SUITE-ExtraBold.woff2",
+      path: "../assets/fonts/Suite/SUITE-ExtraBold.woff2",
       weight: "800",
       style: "normal",
     },
     {
-      path: "./fonts/Suite/SUITE-Heavy.woff2",
+      path: "../assets/fonts/Suite/SUITE-Heavy.woff2",
       weight: "900",
       style: "normal",
     },
     {
-      path: "./fonts/Suite/SUITE-Variable.woff2",
+      path: "../assets/fonts/Suite/SUITE-Variable.woff2",
       weight: "300 900",
       style: "normal",
     },
@@ -50,7 +50,7 @@ export const suite = localFont({
 export const yeongdeokSea = localFont({
   src: [
     {
-      path: "./fonts/YeongdeokSea/YeongdeokSea.ttf",
+      path: "../assets/fonts/YeongdeokSea/YeongdeokSea.ttf",
       weight: "400",
       style: "normal",
     },
