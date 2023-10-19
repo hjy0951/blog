9dd73e2ae0986a85cfb5c74c5f0a85f58b19830c
hjy0951 <hjy0951@gmail.com>
2023-10-20 04:02:00 +0900

    feat: 블로그 글 추가

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -2,4 +2,8 @@ export const processedTagName: { [prop: string]: string } = {
   all: "모든 글",
   javascript: "JavaScript",
   retrospect: "회고",
+  tanstackQuery: "Tanstack-Query",
+  recoil: "Recoil",
+  cicd: "CI/CD",
+  develop: "개발",
 };
