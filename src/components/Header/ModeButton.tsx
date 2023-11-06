f9d24eb617892ce847d83311a46650978a15fe8b
hjy0951 <hjy0951@gmail.com>
2023-11-06 23:17:03 +0900

    feat: 다크모드 변경을 쿠키를 사용한 방식으로 변경

--- src/components/Header/ModeButton.tsx
+++ src/components/Header/ModeButton.tsx
@@ -1,14 +1,38 @@
 "use client";
-import React from "react";
-import useDarkMode from "@/hooks/useDarkMode";
+import React, { useEffect, useState } from "react";
+import Cookies from "js-cookie";
 
 const ModeButton = () => {
-  const [isDark, darkModeButtonHandler] = useDarkMode();
+  const [mounted, setMounted] = useState(false);
+  const [isDark, setIsDark] = useState<boolean>(
+    Cookies.get("theme") === "dark"
+  );
+
+  const modeButtonHandler = () => {
+    const themeToChange = !isDark ? "dark" : "light";
+    Cookies.set("theme", themeToChange, { expires: 1000 });
+    setIsDark((prev) => !prev);
+  };
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (isDark) {
+      document?.documentElement.classList.add("dark");
+    } else {
+      document?.documentElement.classList.remove("dark");
+    }
+  }, [isDark]);
+
+  if (!mounted) return null;
+
   return (
     <button
       className="dark:text-white"
       type="button"
-      onClick={darkModeButtonHandler}
+      onClick={modeButtonHandler}
     >
       Mode
     </button>
