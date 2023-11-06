ad086a216021eb325029d6835165896680df6a6f
hjy0951 <hjy0951@gmail.com>
2023-11-07 00:17:26 +0900

    feat: 다크모드 버튼 완성

--- src/components/Header/ModeButton.tsx
+++ src/components/Header/ModeButton.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Cookies from "js-cookie";
+import { motion } from "framer-motion";
+import { RiMoonFill, RiQuestionMark, RiSunFill } from "react-icons/ri";
 
 const ModeButton = () => {
   const [mounted, setMounted] = useState(false);
@@ -26,16 +28,33 @@ const ModeButton = () => {
     }
   }, [isDark]);
 
-  if (!mounted) return null;
+  if (!mounted)
+    return (
+      <div className="p-1 rounded-md border">
+        <RiQuestionMark size={24} />
+      </div>
+    );
 
   return (
-    <button
-      className="dark:text-white"
+    <motion.button
+      className="p-1.5 rounded-md dark:text-white bg-sky-300 dark:bg-black"
       type="button"
       onClick={modeButtonHandler}
+      whileTap={{ scale: 0.95, rotate: 25 }}
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        opacity: 1,
+        rotate: 360,
+      }}
     >
-      Mode
-    </button>
+      {isDark ? (
+        <RiMoonFill size={22} color="#FFE400" />
+      ) : (
+        <RiSunFill size={22} color="#FF0000" />
+      )}
+    </motion.button>
   );
 };
 
